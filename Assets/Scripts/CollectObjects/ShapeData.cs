8f790df81869eee41834eb99cce825d631138ca6
basarYargici <ibrahimbyargici@gmail.com>
2020-10-03 03:31:49 +0300

    Add ShapeData

---
+++ Assets/Scripts/CollectObjects/ShapeData.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace CollectObjects
+{
+    public class ShapeData
+    {
+        public int id { get; }
+        public Sprite shapeSprite { get; }
+
+        public ShapeData(int id, Sprite sprite)
+        {
+            this.id = id;
+            shapeSprite = sprite;
+        }
+    }
+}
